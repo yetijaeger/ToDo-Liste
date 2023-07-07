170ed5fa2a5712dd699842139222a626e3a09ffb
C_Dirauf <cd@dc.ag>
2023-07-07 08:08:25 +0200

    Refactor

--- src/main/java/com/example/todo/HelloApplication.java
+++ src/main/java/com/example/todo/HelloApplication.java
@@ -1,23 +1,23 @@
 package com.example.todo;
+
 import com.example.todo.todo.ToDo;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import java.io.IOException;
+
 import java.time.LocalDateTime;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application
+{
     @Override
     public void start(Stage stage)
     {
-// Data
+        // Data
         ObservableList<ToDo> data = FXCollections.observableArrayList(
                 new ToDo(1, "Schnitzel kaufen", LocalDateTime.now(), LocalDateTime.now()),
                 new ToDo(2, "Salami kaufen", LocalDateTime.now(), null),
@@ -26,7 +26,7 @@ public class HelloApplication extends Application {
                 new ToDo(5, "Sport", LocalDateTime.now(), null)
         );
 
-// Create table
+        // Create table
         TableView<ToDo> tableView = new TableView<>();
 
         TableColumn<ToDo, String> nameColumn = new TableColumn<>("ID");
@@ -41,23 +41,25 @@ public class HelloApplication extends Application {
         TableColumn<ToDo, Integer> finishColum = new TableColumn<>("Finish");
         finishColum.setCellValueFactory(new PropertyValueFactory<>("finishDate"));
 
-// Add columns to the TableView
+        // Add columns to the TableView
         tableView.getColumns().add(nameColumn);
         tableView.getColumns().add(descriptionColum);
         tableView.getColumns().add(creationColum);
         tableView.getColumns().add(finishColum);
 
-// Set the data to the TableView
+        // Set the data to the TableView
         tableView.setItems(data);
 
-// Create a scene and add the TableView to it
+        // Create a scene and add the TableView to it
         Scene scene = new Scene(tableView);
 
-// Set the scene to the stage and show the stage
+        // Set the scene to the stage and show the stage
         stage.setScene(scene);
         stage.show();
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         launch();
     }
 }
