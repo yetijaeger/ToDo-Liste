af5ca5356afb5e20d9bea4925231e79a7137d3fb
C_Dirauf <cd@dc.ag>
2023-07-19 10:50:54 +0200

    Did stuff

--- src/main/java/com/example/todo/todo/SQLiteConnection.java
+++ src/main/java/com/example/todo/todo/SQLiteConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLiteConnection {
-    private static final String DB_URL = "jdbc:sqlite:src/main/java/com/example/todo/todo/database.db";
+    private static final String DB_URL = "jdbc:sqlite:src/main/java/com/example/todo/todo/ToDo.db";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL);
