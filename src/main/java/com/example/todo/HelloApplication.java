26644cb9627ffdd3e8e640484379ba7fb112bd0e
orang.tuan <tuan-anh@gmx.de>
2023-07-19 09:36:38 +0200

    display table entries with additional delete function

--- src/main/java/com/example/todo/HelloApplication.java
+++ src/main/java/com/example/todo/HelloApplication.java
@@ -1,35 +1,36 @@
 package com.example.todo;
 
+import com.example.todo.todo.DeleteEntry;
+import com.example.todo.todo.GetEntries;
 import com.example.todo.todo.ToDo;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class HelloApplication extends Application
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws SQLException {
         launch();
     }
 
     @Override
-    public void start(Stage stage)
-    {
-        // DataDefiniton
-        ObservableList<ToDo> data = FXCollections.observableArrayList(
-                new ToDo(1, "Schnitzel kaufen", LocalDateTime.now(), LocalDateTime.now()),
-                new ToDo(2, "Salami kaufen", LocalDateTime.now(), null),
-                new ToDo(3, "Yo mama", LocalDateTime.now(), null),
-                new ToDo(4, "Yolo", LocalDateTime.now(), null),
-                new ToDo(5, "Sport", LocalDateTime.now(), null)
-        );
+    public void start(Stage stage) throws SQLException {
+
+        // Retrieve data from the database
+        List<ToDo> entries = GetEntries.getAllEntries();
+        ObservableList<ToDo> data = FXCollections.observableArrayList(entries);
 
         // Create table
         TableView<ToDo> tableView = new TableView<>();
@@ -55,11 +56,46 @@ public class HelloApplication extends Application
         // Set the data to the TableView
         tableView.setItems(data);
 
+        // Create a ScrollPane and set the TableView as its content
+        ScrollPane scrollPane = new ScrollPane(tableView);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
         // Create a scene and add the TableView to it
-        Scene scene = new Scene(tableView);
+        Scene scene = new Scene(scrollPane);
 
         // Set the scene to the stage and show the stage
         stage.setScene(scene);
+        stage.show();
+
+        // Create the delete button
+        Button deleteButton = new Button("Delete");
+        deleteButton.setOnAction(event -> {
+            ToDo selectedTodo = tableView.getSelectionModel().getSelectedItem();
+            if (selectedTodo != null) {
+                data.remove(selectedTodo);
+                // Delete the entry from the database
+                try {
+                    DeleteEntry.delete(selectedTodo.getId());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // Create a VBox to hold the table and the delete button
+        VBox vbox = new VBox(tableView, deleteButton);
+
+        // Create a ScrollPane and set the VBox as its content
+        ScrollPane scrollPane2 = new ScrollPane(vbox);
+        scrollPane2.setFitToHeight(true);
+        scrollPane2.setFitToWidth(true);
+
+        // Create a scene and add the ScrollPane to it
+        Scene scene2 = new Scene(scrollPane2);
+
+        // Set the scene to the stage and show the stage
+        stage.setScene(scene2);
         stage.show();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.todo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
 
 
     opens com.example.todo to javafx.fxml;
